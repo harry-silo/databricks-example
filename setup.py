187172f5521395ecce765b88e1c2ac676f9b4da4
echaros <charalampos.souris@CharalampossAir.lan>
2021-11-16 23:18:30 +0200

    no project slug

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 from setuptools import find_packages, setup
-from fashion-mnist-dataset.project_slug import __version__
 
 setup(
     name="fashion-mnist-dataset.project_slug",
     packages=find_packages(exclude=["tests", "tests.*"]),
     setup_requires=["wheel"],
-    version=__version__,
+    version=1,
     description="fashion-mnist-dataset.description",
     author="fashion-mnist-dataset.author",
 )
