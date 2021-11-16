ac80f7d47960d0d6546e9a13e6874d565da07146
echaros <charalampos.souris@CharalampossAir.lan>
2021-11-16 21:57:12 +0200

    first try

---
+++ fashion-mnist-dataset/fashion-mnist-dataset/jobs/sample/entrypoint.py
@@ -0,0 +1,25 @@
+from fashion-mnist-dataset.common import Job
+import mlflow
+
+class SampleJob(Job):
+
+    def launch(self):
+        self.logger.info("Launching sample job")
+
+        listing = self.dbutils.fs.ls("dbfs:/")
+
+        for l in listing:
+            self.logger.info(f"DBFS directory: {l}")
+
+        df = self.spark.range(0, 1000)
+
+        df.write.format(self.conf["output_format"]).mode("overwrite").save(
+            self.conf["output_path"]
+        )
+        
+        self.logger.info("Sample job finished!")
+        mlflow.set_experiment('first push')
+
+if __name__ == "__main__":
+    job = SampleJob()
+    job.launch()
